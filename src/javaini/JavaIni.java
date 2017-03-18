75641c7523e24fc5ff4a2ec4b84af1946e99afdb
pantherChi <vithursan@email.com>
2017-03-18 15:16:55 +0000

    Now I can get the date value from the user and have it stored inside myArray[]

--- src/javaini/JavaIni.java
+++ src/javaini/JavaIni.java
@@ -5,49 +5,30 @@
  */
 package javaini;
 import java.util.Scanner;
-/**
- *
- * @author vithursan
- */
 public class JavaIni {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-		//Decide the number of friends
-		System.out.print("Enter how many friends: ");
-		int numOfFriends = Integer.parseInt(scan.nextLine());
- 
-		//Create a string array to store the names of your friends
-		String arrayOfNames[] = new String[numOfFriends];
-		for (int i = 0; i < arrayOfNames.length; i++) {
-			System.out.print("Enter the name of friend " + (i+1) + " : ");
-		        arrayOfNames[i] = scan.nextLine();
-		}
-		    
-		//Now show your friend's name one by one
-		for (int i = 0; i < arrayOfNames.length; i++) {
-			System.out.print("My friend " + (i+1) + " : ");
-		        System.out.print(arrayOfNames[i] + "\n");
-		}
-        // TODO code application logic here
-//        System.out.println("Hello, World!");
-//        Scanner userInput = new Scanner(System.in);
-//        String inp = userInput.nextLine();
-//        System.out.println("Hello, " + inp);
-//        
-//        for(int i=10; i>1; i--){
-//              System.out.println("The value of i is: "+i);
-//         }
-//        int [] numbers = {10, 20, 30, 40, 50};
-//        
-//        for(String i : userInput){
-//            System.out.println(i);
-//        }
-//        inputList = userInput.skip("/")[]
+        int[] myArray = dateArray();
+        printArray(myArray);
+    }
+    public static int[] dateArray(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your date of birth (dd/mm/yyyy): ");
+        String input = sc.nextLine();
+        String[] stringArray = input.split("/");
+        final int SIZE = stringArray.length;
+        int[] intArray = new int[SIZE];
+        
+        for (int i = 0; i < SIZE; i++) {
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+        sc.close();
+        return intArray;
     }
     
+    public static void printArray(int[] array){
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
 }
